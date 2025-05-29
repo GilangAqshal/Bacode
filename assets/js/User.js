87a7259c191730935ad8f1369fbe0e61c601424b
GilangAqshal <gilangais16@gmail.com>
2025-05-29 13:56:33 +0700

    First Commit & login

---
+++ assets/js/User.js
@@ -0,0 +1,112 @@
+    // Sebagai Model = PEnyimpanan data base
+    class User{
+        
+        constructor(){
+            this._users = null;
+        }
+
+        getUsers(){
+            if(this._users === null){
+                try{
+                    const storedUser = localStorage.getItem('users');
+                    this._users = storedUser ? JSON.parse(storedUser) : [];
+                }catch(error){
+                    return this._users = [];
+                }
+            }
+            return this._users;
+        }
+
+        // Melakukan Proses Validsi
+        savedUser(userData){
+            const {name, username, avatar, password} = userData;
+            
+            if(typeof name !== 'string' || name.trim() === ''){
+                return{
+                    success: false,
+                    error: 'name is missing'
+                }
+            }
+
+            if(typeof username !== 'string' || username.trim() === ''){
+                return{
+                    success: false,
+                    error: 'username is missing'
+                }
+            }
+
+            if(typeof avatar !== 'string' || avatar.trim() === ''){
+                return{
+                    success: false,
+                    error: 'avatar is missing'
+                }
+            }
+
+            if(typeof password !== 'string' || password.trim() === ''){
+                return{
+                    success: false,
+                    error: 'password is missing'
+                }
+            }
+
+            if(password.length < 6){
+                return{
+                    success: false,
+                    error: 'Password setidaknya, harus diisi dengan 6 karakter'
+                }
+            }
+
+            const newUser = {
+                id: Date.now(),
+                isActive: true,
+                ...userData
+            };
+
+            const users = this.getUsers();
+            users.push(newUser);
+
+            try{
+                localStorage.setItem('users',JSON.stringify(users));
+                return{
+                    success: true,
+                }
+            }catch(error){
+                return{
+                    success: false,
+                }
+            }
+
+        }
+
+        userSignIn(userData){
+            const { username,password } = userData;
+
+            if (typeof username !== "string" || username.trim() === "") {
+            return {
+                success: false,
+                error: 'username is missing'
+            };
+            }
+
+            if (typeof password !== "string" || password.trim() === "") {
+            return {
+                success: false,
+                error: 'password is missing'
+            }
+            }
+
+            const userExists = this.getUsers().some(user => user.username.toLowerCase() === username.toLowerCase() && user.password === password);
+
+            if(userExists){
+                return{
+                    success: true,
+                }
+            }else{
+                return{
+                    success:false,
+                    error:'Username atau Password Salah!'
+                }
+            }
+
+        }
+    }
---
+++ assets/js/addUser.js
@@ -0,0 +1,44 @@
+// Sebagai Controller = Penghubung Model dan UI
+document.addEventListener('DOMContentLoaded', () => {
+
+        const formManager = document.getElementById('formManager');
+        const userName = document.getElementById('name');
+        const userAvatar = document.getElementById('avatar');
+        const userUsername = document.getElementById('username');
+        const userPassword = document.getElementById('password');
+
+        const instantFeedback = document.getElementById('instantFeedback');
+
+        const userManager = new User();
+
+        // Membuat format tanggal dengan "yyyy-mm-dd"
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2,'0');
+
+        formManager.addEventListener('submit', (event) =>{
+
+            event.preventDefault();
+
+            const userData = {
+                name: userName.value,
+                username: userUsername.value,
+                avatar: userAvatar.value,
+                password: userPassword.value,
+                createdAt: '${year}-${month}-${day}',
+            };
+
+            const result = userManager.savedUser(userData);
+
+            if(result.success){
+                instantFeedback.style.display = 'none';
+                // arahkan pengguna kepada halaman lain yaitu login
+                return window.location.href = '../login.html';
+            }else{
+                instantFeedback.style.display = 'flex';
+                instantFeedback.textContent = result.error;
+            }
+        })
+
+});
